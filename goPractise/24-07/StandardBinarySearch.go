123524ac369be553cc3318b9a1908e43c9be2b8f
Jekaterina Macilevic <jekaterinam@Jekaterinas-MBP.mynet>
2025-07-24 10:32:47 +0100

    Standard Binary search + recursive quick sort

---
+++ goPractise/24-07/StandardBinarySearch.go
@@ -0,0 +1,27 @@
+package main
+
+func binarySearch(nums []int, target int) int {
+	low := nums[0]
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
+
+// func main() {
+// 	numbers := []int{2, 3, 7, 9, 13, 17}
+// 	target := 17
+// 	answer := binarySearch(numbers, target)
+
+// 	fmt.Println("the answer is:", answer)
+// }
---
+++ goPractise/24-07/quickSort.go
@@ -0,0 +1,43 @@
+// swap - It avoids creating extra arrays, so it’s space-efficient (O(1) extra space)
+// we are always operating on the original array
+
+package main
+
+import "fmt"
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[high]
+	i := low - 1
+
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			i++
+			// swapping arr[j] and arr[i]
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+
+	//swapping pivot next to the numbers less than pivot
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+
+	return i + 1
+}
+
+// quickSort sorts the array in-place indices low and high
+func quickSort(arr []int, low, high int) {
+	if low < high {
+		// partition the array to get a pivot index
+		pivotIndex := partition(arr, low, high)
+
+		// Now devide this into left and right - recursion
+		quickSort(arr, low, pivotIndex-1)
+		quickSort(arr, pivotIndex+1, high)
+	}
+}
+
+func main() {
+	nums := []int{4, 2, 8, 4, 1, 5, 3, 9, 13, 56, 3}
+	fmt.Println("Before:", nums)
+	quickSort(nums, 0, len(nums)-1)
+	fmt.Println("After:", nums)
+}
