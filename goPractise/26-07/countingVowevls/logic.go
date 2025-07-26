02bf51f9f5efb99d773a6f330a4ebcd2d0ef39dc
Jekaterina Macilevic <jekaterinam@Jekaterinas-MBP.mynet>
2025-07-26 23:23:23 +0100

    Added writing to file at that exmaple

--- goPractise/26-07/countingVowevls/logic.go
+++ goPractise/26-07/countingVowevls/logic.go
@@ -42,14 +42,46 @@ func countVowels(s string) int {
 	return count
 }
 
+// Writing this info to a new json file
+type UserVowels struct {
+	Name       string `json:"name"`
+	VowelCount int    `json:"vowel_count"`
+}
+
+func writeToFile(fileName string, users []UserVowels) error {
+	data, err := json.MarshalIndent(users, "", " ")
+
+	if err != nil {
+		fmt.Println("Error marhasling from struct to json")
+		return err
+	}
+
+	if err := os.WriteFile(fileName, data, 0644); err != nil {
+		fmt.Println("Error writing to file")
+		return err
+	}
+	return nil
+}
+
 func main() {
 	file := "./file.json"
 	data := unmarshalJson(file)
 
 	fmt.Println(data)
 
+	var results []UserVowels
 	for _, user := range data {
 		number := countVowels(user.Name)
 		fmt.Printf("The user %s has %d vowels in their name\n", user.Name, number)
+
+		results = append(results, UserVowels{
+			Name:       user.Name,
+			VowelCount: number,
+		})
+	}
+	if err := writeToFile("results.json", results); err != nil {
+		fmt.Println("Error writting to file: ", err)
+	} else {
+		fmt.Println("Results are written to results.json")
 	}
 }
